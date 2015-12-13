4482df44f1f6b8b13f9bab139ed08f35a31ddcea
rikyuusima <equanz324@gmail.com>
2015-12-14 01:28:31 +0900

    fix インターネットアクセスが無い時

--- Web_test/app/src/main/java/mm/web_test/WebActivity.java
+++ Web_test/app/src/main/java/mm/web_test/WebActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class WebActivity extends AppCompatActivity {
-    String fURL ="http://www.google.co.jp/";
+    String fURL ="http://rsserver.web.fc2.com/";
     //static String n = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +36,24 @@ public class WebActivity extends AppCompatActivity {
         WebView webView01 = new WebView(this);
         webView01.getSettings().setJavaScriptEnabled(false);
         webView01.loadUrl(fURL);
-
+        long start = System.currentTimeMillis();
+        long end;
         do {
             a = webView01.getTitle();
+            end = System.currentTimeMillis();
+            if(5000<=(end-start)) {
+                a = "hoge";
+                break;
+            }
         }while (a == null);
         //n =a;
 
         //tv.setText("hoge"+a+"hoge");
 
-        if(a.equals("Webpage not available")){
-            return false;
-        }else{
+        if(a.equals("rsserver")){
             return true;
+        }else{
+            return false;
         }
     }
 }
